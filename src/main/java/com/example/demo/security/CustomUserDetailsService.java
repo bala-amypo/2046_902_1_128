9605893af11e256346060d2ef5b4ca247fba8ed4
bala-amypo <bala@amypo.in>
2025-12-20 13:04:23 +0000

    User commit

--- src/main/java/com/example/demo/security/CustomUserDetailsService.java
+++ src/main/java/com/example/demo/security/CustomUserDetailsService.java
@@ -1,30 +1,30 @@
 package com.example.demo.security;
 
-import com.example.demo.entity.UserAccount;
-import com.example.demo.repository.UserAccountRepository;
-import org.springframework.security.core.userdetails.*;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
+// // import com.example.demo.entity.UserAccount;
+// // import com.example.demo.repository.UserAccountRepository;
+// // import org.springframework.security.core.userdetails.*;
+// // import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
-import java.util.Collections;
+// import java.util.Collections;
 
-public class CustomUserDetailsService implements UserDetailsService {
+public class CustomUserDetailsService  {
 
-    private final UserAccountRepository repo;
+    // private final UserAccountRepository repo;
 
-    public CustomUserDetailsService(UserAccountRepository repo) {
-        this.repo = repo;
-    }
+    // public CustomUserDetailsService(UserAccountRepository repo) {
+    //     this.repo = repo;
+    // }
 
-    @Override
-    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+    // @Override
+    // public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
 
-        UserAccount user = repo.findByEmail(email)
-                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+    //     UserAccount user = repo.findByEmail(email)
+    //             .orElseThrow(() -> new UsernameNotFoundException("User not found"));
 
-        return new User(
-                user.getEmail(),
-                user.getPassword(),
-                Collections.singletonList(new SimpleGrantedAuthority("ROLE_" + user.getRole().name()))
-        );
-    }
+    //     return new User(
+    //             user.getEmail(),
+    //             user.getPassword(),
+    //             Collections.singletonList(new SimpleGrantedAuthority("ROLE_" + user.getRole().name()))
+    //     );
+    // }
 }
