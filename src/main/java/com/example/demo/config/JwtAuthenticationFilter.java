9b5651a9d62601d43c6e4b6e3fcfee101be5e7b9
bala-amypo <bala@amypo.in>
2025-12-20 12:55:23 +0000

    User commit

--- src/main/java/com/example/demo/config/JwtAuthenticationFilter.java
+++ src/main/java/com/example/demo/config/JwtAuthenticationFilter.java
@@ -1,48 +1,7 @@
-// package com.example.demo.security;
+package com.example.demo.config;
 
-// import io.jsonwebtoken.Claims;
-// import org.springframework.security.core.context.SecurityContextHolder;
-// import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-// import javax.servlet.*;
-// import javax.servlet.http.*;
-// import java.io.IOException;
-// import java.util.Collections;
 
-public class JwtAuthenticationFilter extends GenericFilter {
 
-    // private final JwtUtil jwtUtil;
+public class JwtAuthenticationFilter  {
 
-    // public JwtAuthenticationFilter(JwtUtil jwtUtil) {
-    //     this.jwtUtil = jwtUtil;
-    // }
-
-    // @Override
-    // public void doFilter(
-    //         ServletRequest req,
-    //         ServletResponse res,
-    //         FilterChain chain) throws IOException, ServletException {
-
-    //     HttpServletRequest request = (HttpServletRequest) req;
-    //     String header = request.getHeader("Authorization");
-
-    //     if (header != null && header.startsWith("Bearer ")) {
-
-    //         String token = header.substring(7);
-
-    //         if (!jwtUtil.isExpired(token)) {
-    //             Claims claims = jwtUtil.extractClaims(token);
-
-    //             UsernamePasswordAuthenticationToken auth =
-    //                     new UsernamePasswordAuthenticationToken(
-    //                             claims.get("email"),
-    //                             null,
-    //                             Collections.emptyList()
-    //                     );
-
-    //             SecurityContextHolder.getContext().setAuthentication(auth);
-    //         }
-    //     }
-
-    //     chain.doFilter(req, res);
-    // }
-}
+    
