d24690f9cf6313bf425f00ea0557a0c999d5e125
bala-amypo <bala@amypo.in>
2025-12-20 12:01:14 +0000

    User commit

--- src/main/java/com/example/demo/service/AllocationRuleService.java
+++ src/main/java/com/example/demo/service/AllocationRuleService.java
@@ -1,3 +1,19 @@
-public class AllocationRuleService{
-    
+package com.example.demo.service;
+
+import com.example.demo.entity.AssetClassAllocationRule;
+import java.util.List;
+
+public interface AllocationRuleService {
+
+    AssetClassAllocationRule createRule(AssetClassAllocationRule rule);
+
+    AssetClassAllocationRule updateRule(Long id, AssetClassAllocationRule updatedRule);
+
+    List<AssetClassAllocationRule> getRulesByInvestor(Long investorId);
+
+    List<AssetClassAllocationRule> getActiveRules(Long investorId);
+
+    AssetClassAllocationRule getRuleById(Long id);
+
+    void deleteRule(Long id);
 }
