dee5479e9417abd210438ae87252eccaefbdb92c
bala-amypo <bala@amypo.in>
2025-12-20 12:13:23 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/RebalancingAlertRecordServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/RebalancingAlertRecordServiceImpl.java
@@ -1,3 +1,48 @@
-public class RebalancingAlertRecordServiceImpl{
-    
+package com.example.demo.service.impl;
+
+import com.example.demo.entity.RebalancingAlertRecord;
+import com.example.demo.exception.ResourceNotFoundException;
+import com.example.demo.repository.RebalancingAlertRecordRepository;
+import com.example.demo.service.RebalancingAlertService;
+
+import java.util.List;
+
+public class RebalancingAlertServiceImpl implements RebalancingAlertService {
+
+    private final RebalancingAlertRecordRepository repo;
+
+    public RebalancingAlertServiceImpl(RebalancingAlertRecordRepository repo) {
+        this.repo = repo;
+    }
+
+    @Override
+    public RebalancingAlertRecord createAlert(RebalancingAlertRecord alert) {
+        if (alert.getCurrentPercentage() <= alert.getTargetPercentage()) {
+            throw new IllegalArgumentException("currentPercentage > targetPercentage required");
+        }
+        return repo.save(alert);
+    }
+
+    @Override
+    public RebalancingAlertRecord resolveAlert(Long id) {
+        RebalancingAlertRecord alert = getAlertById(id);
+        alert.setResolved(true);
+        return repo.save(alert);
+    }
+
+    @Override
+    public List<RebalancingAlertRecord> getAlertsByInvestor(Long investorId) {
+        return repo.findByInvestorId(investorId);
+    }
+
+    @Override
+    public RebalancingAlertRecord getAlertById(Long id) {
+        return repo.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Alert not found"));
+    }
+
+    @Override
+    public List<RebalancingAlertRecord> getAllAlerts() {
+        return repo.findAll();
+    }
 }
