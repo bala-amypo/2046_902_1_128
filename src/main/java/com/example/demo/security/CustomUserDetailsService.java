c7c342e111dfad62162c3ca9d40f9d728f43e788
bala-amypo <bala@amypo.in>
2025-12-23 05:00:18 +0000

    User commit

--- src/main/java/com/example/demo/security/CustomUserDetailsService.java
+++ src/main/java/com/example/demo/security/CustomUserDetailsService.java
@@ -1,30 +1,5 @@
 package com.example.demo.security;
 
-// // import com.example.demo.entity.UserAccount;
-// // import com.example.demo.repository.UserAccountRepository;
-// // import org.springframework.security.core.userdetails.*;
-// // import org.springframework.security.core.authority.SimpleGrantedAuthority;
-
-// import java.util.Collections;
-
 public class CustomUserDetailsService  {
 
-    // private final UserAccountRepository repo;
-
-    // public CustomUserDetailsService(UserAccountRepository repo) {
-    //     this.repo = repo;
-    // }
-
-    // @Override
-    // public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-
-    //     UserAccount user = repo.findByEmail(email)
-    //             .orElseThrow(() -> new UsernameNotFoundException("User not found"));
-
-    //     return new User(
-    //             user.getEmail(),
-    //             user.getPassword(),
-    //             Collections.singletonList(new SimpleGrantedAuthority("ROLE_" + user.getRole().name()))
-    //     );
-    // }
 }
