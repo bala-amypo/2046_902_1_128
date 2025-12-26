ac14b60d8ac94bc8b24a837a35a55eddceab87ff
bala-amypo <bala@amypo.in>
2025-12-26 04:09:06 +0000

    User commit

--- src/main/java/com/example/demo/security/JwtUtil.java
+++ src/main/java/com/example/demo/security/JwtUtil.java
@@ -1,55 +1,55 @@
-package com.example.demo.security;
+// package com.example.demo.security;
 
-import io.jsonwebtoken.*;
-import io.jsonwebtoken.security.Keys;
-import org.springframework.stereotype.Component;
+// import io.jsonwebtoken.*;
+// import io.jsonwebtoken.security.Keys;
+// import org.springframework.stereotype.Component;
 
-import java.security.Key;
-import java.util.Date;
+// import java.security.Key;
+// import java.util.Date;
 
-@Component
+// @Component
 public class JwtUtil {
 
-    private final String SECRET = "ThisIsAStrongJwtSecretKeyForDemoProject12345";
-    private final long EXPIRATION = 1000 * 60 * 60; // 1 hour
+    // private final String SECRET = "ThisIsAStrongJwtSecretKeyForDemoProject12345";
+    // private final long EXPIRATION = 1000 * 60 * 60; // 1 hour
 
-    private Key getSigningKey() {
-        return Keys.hmacShaKeyFor(SECRET.getBytes());
-    }
+    // private Key getSigningKey() {
+    //     return Keys.hmacShaKeyFor(SECRET.getBytes());
+    // }
 
-    public String generateToken(String username) {
+    // public String generateToken(String username) {
 
-        return Jwts.builder()
-                .setSubject(username)
-                .setIssuer("DemoApp")
-                .setIssuedAt(new Date())
-                .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION))
-                .signWith(getSigningKey(), SignatureAlgorithm.HS256)
-                .compact();
-    }
+    //     return Jwts.builder()
+    //             .setSubject(username)
+    //             .setIssuer("DemoApp")
+    //             .setIssuedAt(new Date())
+    //             .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION))
+    //             .signWith(getSigningKey(), SignatureAlgorithm.HS256)
+    //             .compact();
+    // }
 
-    public String extractUsername(String token) {
+    // public String extractUsername(String token) {
 
-        return Jwts.parserBuilder()
-                .setSigningKey(getSigningKey())
-                .build()
-                .parseClaimsJws(token)
-                .getBody()
-                .getSubject();
-    }
+    //     return Jwts.parserBuilder()
+    //             .setSigningKey(getSigningKey())
+    //             .build()
+    //             .parseClaimsJws(token)
+    //             .getBody()
+    //             .getSubject();
+    // }
 
-    public boolean validateToken(String token) {
+    // public boolean validateToken(String token) {
 
-        try {
-            Jwts.parserBuilder()
-                    .setSigningKey(getSigningKey())
-                    .build()
-                    .parseClaimsJws(token);
+    //     try {
+    //         Jwts.parserBuilder()
+    //                 .setSigningKey(getSigningKey())
+    //                 .build()
+    //                 .parseClaimsJws(token);
 
-            return true;
+    //         return true;
 
-        } catch (Exception ex) {
-            return false;
-        }
-    }
+    //     } catch (Exception ex) {
+    //         return false;
+    //     }
+    // }
 }
