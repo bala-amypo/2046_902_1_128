e7c33262c5f73dec74238c1e6c57c605d98c7941
bala-amypo <bala@amypo.in>
2025-12-27 04:09:39 +0000

    User commit

--- src/main/java/com/example/demo/config/JwtTokenProvider.java
+++ src/main/java/com/example/demo/config/JwtTokenProvider.java
@@ -5,6 +5,7 @@ import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
 
@@ -17,7 +18,8 @@ public class JwtTokenProvider {
     private final SecretKey key;
     private final long validityInMs;
     
-    public JwtTokenProvider(String secret, long validityInMs) {
+    public JwtTokenProvider(@Value("${jwt.secret}") String secret, 
+                           @Value("${jwt.expiration}") long validityInMs) {
         this.key = Keys.hmacShaKeyFor(secret.getBytes());
         this.validityInMs = validityInMs;
     }
